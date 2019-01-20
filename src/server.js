fd5602d19f2cc24c87f45701c8e3fd6c7baf2c53
Blast06 <lacalculadora06@hotmail.com>
2019-01-20 13:15:58 -0400

    heroku node set up

--- src/server.js
+++ src/server.js
@@ -4,12 +4,12 @@ const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/firebasepanel'));
+// Serve only the static files form the angularapp directory
+app.use(express.static(__dirname + '/firebasepanel'));
 
 app.get('/*', function(req, res) {
 
-    res.sendFile(path.join(__dirname, '/dist/firebasepanel/index.html'));
+    res.sendFile(path.join(__dirname + '/firebasepanel/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
